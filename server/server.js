d730c57f3522bef3fab5f5206ab32e7edfa1c82c
BJ Pennington <bjpennington112@gmail.com>
2019-04-13 21:41:35 -0500

    set up basic vue instance

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+
+app.listen(port, function() {
+    console.log('Listening on port', port);
+});
