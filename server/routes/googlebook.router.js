0536c8fa9d02bd746daec55dae47c32b9037eb59
BJ Pennington <bjpennington112@gmail.com>
2019-06-12 12:12:04 -0500

    complete googlebook api get request route

--- server/routes/googlebook.router.js
+++ server/routes/googlebook.router.js
@@ -4,10 +4,22 @@ const router = express.Router();
 const axios = require('axios');
 
 router.get('/', (req, res) => {
+    let apiQuery = 'https://www.googleapis.com/books/v1/volumes?q=';
+    let queryParams =  '';
+    let searchRequest = req.query.searchTerms.split(" ");
     let newBook = {};
-    axios.get('https://www.googleapis.com/books/v1/volumes?q=_vKs2MVSxwIC')
+
+    // loop through array of search request words to concatenate query string
+    for (let i = 0; i < searchRequest.length; i++) {
+        queryParams = queryParams + '+' + searchRequest[i];
+    }
+    // remove leading '+' from query string
+    queryParams = queryParams.substring(1);
+    // add query string to url
+    apiQuery = apiQuery += queryParams;
+
+    axios.get(apiQuery)
         .then(response => {
-            console.log(response.data);
             newBook = response.data;
             res.send(newBook);
         })
